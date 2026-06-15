1cc98a03107c373e66c0efc9cdaece485cc081f9
Dishen <ajsty81@gmail.com>
2026-06-15 17:48:57 -0400

    test file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/ServoTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class ServoTest extends OpMode {
+
+    private Servo testServo;
+
+    @Override
+    public void init() {
+        testServo = hardwareMap.get(Servo.class, "testServo");
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.dpad_left) {
+        testServo.setPosition(0.0);
+        }
+        if (gamepad1.dpad_right) {
+            testServo.setPosition(0.8);
+        }
+    }
+
+
+}
